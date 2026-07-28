99b800d5c8c38353e057e82dfdcb2c0a6ad2d34e
wohstheatre <wohsplaybill@gmail.com>
2026-07-28 14:28:06 -0400

    Update config.js

--- wohs-playbill-v2/js/config.js
+++ wohs-playbill-v2/js/config.js
@@ -10,7 +10,7 @@ window.WOHS_CONFIG = {
 
   // Paste the Google Sheets 'Publish to web' CSV link here.
   // Until you do, the search page will use the sample data file included in this folder.
-  trackerCsvUrl: "",
+  trackerCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRVCTX956bNCl3PPrFbIE0E5ZsiWobWDExX9rSXbGyG24hWxntvl8ZC5A4-3tPUWyCoDyq8nKt064LH/pub?gid=942913499&single=true&output=csv",
 
   claimFormUrl: "PASTE_YOUR_CLAIM_FORM_LINK_HERE",
   updateFormUrl: "PASTE_YOUR_UPDATE_FORM_LINK_HERE",
