e2d29a7f912b7bc8d5f1f586b90bee25a31d93b8
WOHSPlaybill <wohsplaybill@gmail.com>
2026-07-31 15:49:45 -0400

    Update config.js
    Update Form URL

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ window.WOHS_CONFIG = {
   trackerCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_bDAU-2IPpat9xXTRwm_kW3tst4RTI4-dDz5AYu1QCa9t3obLAfrM7YDdGMRWVcc9yo3PZQRvo9go/pub?gid=374148145&single=true&output=csv",
 
   claimFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe6Mwl8awzsPveYzsYJV7ZaPNOU4cKWbaickwwKOkgSCxsPJQ/viewform?usp=dialog",
-  updateFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSfI3zBmSRKoKA8hVab8S-4smNS7AAdIL9iy-q2IssL3GFdY3Q/viewform?usp=publish-editor",
+  updateFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSfI3zBmSRKoKA8hVab8S-4smNS7AAdIL9iy-q2IssL3GFdY3Q/viewform?usp=dialog",
   submitSaleFormUrl: "PASTE_YOUR_SALE_FORM_LINK_HERE",
   sponsorshipPacketUrl: "PASTE_YOUR_SPONSORSHIP_PACKET_LINK_HERE",
   artworkUploadUrl: "PASTE_YOUR_ARTWORK_UPLOAD_LINK_HERE",
